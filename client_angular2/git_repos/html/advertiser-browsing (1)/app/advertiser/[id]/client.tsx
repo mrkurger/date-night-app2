476fe3978fd058e4b3d8f3cf791da21f2698e6c3
GitHub Action <action@github.com>
2025-05-29 21:43:03 +0200

    Remove unused PostCSS and Tailwind configuration files, along with the Advertiser Carousel component and its associated styles and logic. This cleanup eliminates unnecessary code and files, streamlining the project structure. Additionally, delete the USB monitor script as it is no longer needed.

--- client_angular2/git_repos/html/advertiser-browsing (1)/app/advertiser/[id]/client.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { AdvertiserDetailWrapper } from "./wrapper"
-
-export default function AdvertiserDetailClient({ id }: { id: string }) {
-  return <AdvertiserDetailWrapper id={id} />
-}
--- client_angular2/git_repos/html/advertiser-browsing (1)/app/advertiser/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import AdvertiserDetailClient from "./client"
-
-export default function AdvertiserDetailPage({ params }: { params: { id: string } }) {
-  return <AdvertiserDetailClient id={params.id} />
-}
